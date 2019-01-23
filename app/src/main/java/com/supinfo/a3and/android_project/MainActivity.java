37425d33d0d1388fdd95e6954132b60595d85e6d
Unknown <juju692010@live.fr>
2019-01-23 17:40:25 +0100

    Change title,color add a first version of the first page

--- app/src/main/java/com/supinfo/a3and/android_project/MainActivity.java
+++ app/src/main/java/com/supinfo/a3and/android_project/MainActivity.java
@@ -2,6 +2,7 @@ package com.supinfo.a3and.android_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
