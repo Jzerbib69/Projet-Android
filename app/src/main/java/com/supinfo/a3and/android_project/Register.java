859111a5858f712617ebb9aff949831837299118
Unknown <juju692010@live.fr>
2019-01-24 19:55:17 +0100

    Add verification if all edtText are written and add API Class
    Need to continue the API, create all function register, Login, Logout... then relate with page register first

--- app/src/main/java/com/supinfo/a3and/android_project/Register.java
+++ app/src/main/java/com/supinfo/a3and/android_project/Register.java
@@ -3,10 +3,12 @@ package com.supinfo.a3and.android_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity{
+    EditText usernameText, firstNameText, lastNameText,emailText, passwordText;
+    String dataUsername, dataFirstName, dataLastName, dataEmail, dataPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +20,49 @@ public class Register extends AppCompatActivity{
         switch (v.getId()){
             case R.id.btnErased:
                 Erased();
+                break;
             case R.id.btnRegister:
                 Register();
         }
     }
 
+    public void getData(){
+        usernameText = findViewById(R.id.edtUsername);
+        firstNameText = findViewById(R.id.edtFirstName);
+        lastNameText = findViewById(R.id.edtLastName);
+        emailText = findViewById(R.id.edtEmail);
+        passwordText = findViewById(R.id.edtPassword);
+    }
+
+    public void initialiseData(){
+        dataUsername = usernameText.getText().toString();
+        dataFirstName = firstNameText.getText().toString();
+        dataLastName = lastNameText.getText().toString();
+        dataEmail = emailText.getText().toString();
+        dataPassword = passwordText.getText().toString();
+    }
+
     public void Erased(){
-        EditText UsernameText = findViewById(R.id.edtUsername);
-        EditText FirstNameText = findViewById(R.id.edtFirstName);
-        EditText LastNameText = findViewById(R.id.edtLastName);
-        EditText EmailText = findViewById(R.id.edtEmail);
-        EditText PasswordText = findViewById(R.id.edtPassword);
-        EditText VerifPasswordText = findViewById(R.id.edtConfirmPassword);
-        UsernameText.setText("");
-        FirstNameText.setText("");
-        LastNameText.setText("");
-        EmailText.setText("");
-        PasswordText.setText("");
-        VerifPasswordText.setText("");
+        getData();
+        usernameText.setText("");
+        firstNameText.setText("");
+        lastNameText.setText("");
+        emailText.setText("");
+        passwordText.setText("");
     }
 
     public void Register(){
-        
+        getData();
+        initialiseData();
+        if (dataUsername.matches("") || dataFirstName.matches("") || dataLastName.matches("") || dataEmail.matches("")
+                || dataPassword.matches(""))
+        {
+            Toast.makeText(this, "Vous devez renseigner tous les champs !", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String api =
+            Toast.makeText(this, "Inscription réussie", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
