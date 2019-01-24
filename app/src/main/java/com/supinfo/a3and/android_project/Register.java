e8cbbb6b9bac00786a3c3c5f6a5f3bc8994905df
Nathan <nathanmercier@orange.fr>
2019-01-24 18:18:06 +0100

    btn cancel

--- app/src/main/java/com/supinfo/a3and/android_project/Register.java
+++ app/src/main/java/com/supinfo/a3and/android_project/Register.java
@@ -7,28 +7,30 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class Register extends AppCompatActivity{
+    EditText userNameText, firstNameText, lastNameText, emailText, passwordText, verifPasswordText;
+    Button btnCancel, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
     }
-
+    
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnCancel:
-                EditText UsernameText = findViewById(R.id.edtUsername);
-                EditText FirstNameText = findViewById(R.id.edtFirstName);
-                EditText LastNameText = findViewById(R.id.edtLastName);
-                EditText EmailText = findViewById(R.id.edtEmail);
-                EditText PasswordText = findViewById(R.id.edtPassword);
-                EditText VerifPasswordText = findViewById(R.id.edtConfirmPassword);
-                UsernameText.setText("");
-                FirstNameText.setText("");
-                LastNameText.setText("");
-                EmailText.setText("");
-                PasswordText.setText("");
-                VerifPasswordText.setText("");
+                userNameText = (EditText) findViewById(R.id.edtUsername);
+                firstNameText = (EditText) findViewById(R.id.edtFirstName);
+                lastNameText = (EditText) findViewById(R.id.edtLastName);
+                emailText = (EditText) findViewById(R.id.edtEmail);
+                passwordText = (EditText) findViewById(R.id.edtPassword);
+                verifPasswordText = (EditText) findViewById(R.id.edtConfirmPassword);
+                userNameText.setText("");
+                firstNameText.setText("");
+                lastNameText.setText("");
+                emailText.setText("");
+                passwordText.setText("");
+                verifPasswordText.setText("");
             case R.id.btnRegister:
                 /*
                 */
