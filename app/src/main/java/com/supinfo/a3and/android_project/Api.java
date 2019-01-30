a89f1d2e8da99fa2be29d840c941ba00c4587ce3
Nathan <nathanmercier@orange.fr>
2019-01-30 19:15:05 +0100

    Auto stash before merge of "master" and "origin/master"
    login fonctionnel

--- app/src/main/java/com/supinfo/a3and/android_project/Api.java
+++ app/src/main/java/com/supinfo/a3and/android_project/Api.java
@@ -1,6 +1,7 @@
 package com.supinfo.a3and.android_project;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
@@ -9,26 +10,33 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Api extends AppCompatActivity {
 
     boolean state = false;
-    Context context;
+    //WeakReference<Context> contextReference;
     public void register(final String username, final String password, final String firstName, final String lastName, final String email, Context context){
-        this.context = context;
+        //this.contextReference = new WeakReference<Context>(context);
+
         Thread thread = new Thread(new Runnable() {
+
             @Override
             public void run() {
+
                 try {
                     URL url = new URL("http://supinfo.steve-colinet.fr/suptodo?action=register&username=" + username + "&password=" + password + "&firstname=" + firstName + "&lastname=" + lastName + "&email=" + email);
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                     try {
+
                         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
                         StringBuilder stringBuilder = new StringBuilder();
                         String line;
+
                         while ((line = bufferedReader.readLine()) != null) {
+
                             stringBuilder.append(line).append("\n");
                         }
                         bufferedReader.close();
@@ -36,11 +44,10 @@ public class Api extends AppCompatActivity {
                         Log.e("string", stringBuilder.toString());
                         if((""+stringBuilder.charAt(11)+stringBuilder.charAt(12)+stringBuilder.charAt(13)+stringBuilder.charAt(14)+stringBuilder.charAt(15)).equals("false")){
                             //postToastMessage("Echec de l'inscription, utilisateur déjà existant.");
-                            isConnected(false);
                         }
                         else{
-                            login(username, password);
                             isConnected(true);
+                            login(username, password);
                         }
 
                     } finally {
@@ -55,6 +62,9 @@ public class Api extends AppCompatActivity {
     }
 
     public void login(final String username, final String password){
+        /*Context context = this.contextReference.get();
+        this.contextReference = new WeakReference<Context>(context);*/
+
         Thread thread = new Thread(new Runnable() {
 
             @Override
@@ -74,10 +84,14 @@ public class Api extends AppCompatActivity {
 
                         Log.e("string", stringBuilder.toString());
                         if((""+stringBuilder.charAt(11)+stringBuilder.charAt(12)+stringBuilder.charAt(13)+stringBuilder.charAt(14)+stringBuilder.charAt(15)).equals("false")){
-                            isConnected(false);
+                            //postToastMessage("Echec de la connexion, veuillez vérifier vos Identifiants.");
+
                         }
                         else{
+                            Log.e("string","Connexion réussie !");
+                            //postToastMessage("Vous êtes maintenant connecté !");
                             isConnected(true);
+
                         }
 
                     } finally {
@@ -203,6 +217,7 @@ public class Api extends AppCompatActivity {
         else{
             this.state=false;
         }
+        Log.e("string", String.valueOf(this.state));
     }
 
     public boolean isConnected(){
@@ -216,9 +231,54 @@ public class Api extends AppCompatActivity {
 
             @Override
             public void run() {
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                /*Context context = contextReference.get();
+                if(context!=null) {
+                    //Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                }*/
             }
         });
+    }
+
+    public void read(final String username, final String password){
+        /*Context context = this.contextReference.get();
+        this.contextReference = new WeakReference<Context>(context);*/
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://supinfo.steve-colinet.fr/suptodo?action=login&username=" + username + "&password=" + password);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    try {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(line).append("\n");
+                        }
+                        bufferedReader.close();
+
+
+                        Log.e("string", stringBuilder.toString());
+                        if((""+stringBuilder.charAt(11)+stringBuilder.charAt(12)+stringBuilder.charAt(13)+stringBuilder.charAt(14)+stringBuilder.charAt(15)).equals("false")){
+                            //postToastMessage("Echec de la connexion, veuillez vérifier vos Identifiants.");
+                        }
+                        else{
+                            Log.e("string","Inscription réussie !");
+                            //postToastMessage("Vous êtes maintenant connecté !");
+                            //isConnected(true);
+                        }
+
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (Exception e) {
+                    Log.e("ERROR", e.getMessage(), e);
+                }
+            }
+        });
+        thread.start();
     }
 
 }
