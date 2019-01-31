f6a7cfbd1d73ec7bcd18f48966c3255714faf88b
Roussetheo <roussetheo74@gmail.com>
2019-01-31 15:20:08 +0100

    Internet connection check

---
+++ app/src/main/java/com/supinfo/a3and/android_project/Database/RegisterBDD.java
@@ -0,0 +1,39 @@
+package com.supinfo.a3and.android_project.Database;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import com.supinfo.a3and.android_project.R;
+import com.supinfo.a3and.android_project.Register;
+
+
+public class RegisterBDD extends AppCompatActivity {
+
+    EditText usernameText, firstNameText, lastNameText,emailText, passwordText;
+    String dataUsername, dataFirstName, dataLastName, dataEmail, dataPassword;
+
+    public void getData(){
+        usernameText = findViewById(R.id.edtUsernameRegister);
+        firstNameText = findViewById(R.id.edtFirstName);
+        lastNameText = findViewById(R.id.edtLastName);
+        emailText = findViewById(R.id.edtEmail);
+        passwordText = findViewById(R.id.edtPasswordRegister);
+    }
+
+    public void initialiseData(){
+        dataUsername = usernameText.getText().toString();
+        dataFirstName = firstNameText.getText().toString();
+        dataLastName = lastNameText.getText().toString();
+        dataEmail = emailText.getText().toString();
+        dataPassword = passwordText.getText().toString();
+    }
+
+    public void RegisterBDD(){
+        getData();
+        initialiseData();
+
+    }
+
+
+
+}
